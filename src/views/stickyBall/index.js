c28bc9af165e646da72ddafc4e54466149930e38
luckyCao <cao.lei1031@163.com>
2017-02-09 22:59:25 +0800

    拖拽动效

--- src/views/stickyBall/index.js
+++ src/views/stickyBall/index.js
@@ -57,7 +57,7 @@ class StickyBall {
    * touchstart事件
    */
   dragStart(event) {
-    console.log('start')
+    event.preventDefault()
     let pos = this.getMousePos(event)
     if(this.isPosInPath(pos, this.ball, posInCircle)) {
       this.canDragBall = true // touchstart在球上，可以拖动球
@@ -67,6 +67,7 @@ class StickyBall {
    * touchmove事件
    */
   dragMove(event) {
+    event.preventDefault()
     if (this.canDragBall) {
       let pos = stickyBall.getMousePos(event)
       stickyBall.dragBall.pos = pos
@@ -75,6 +76,7 @@ class StickyBall {
     }
   }
   dragEnd(event) {
+    event.preventDefault()
     // 选择缓动函数
     var bounce_animate_type = TWEEN.Easing.Elastic.InOut
     TWEEN.removeAll()
@@ -109,18 +111,46 @@ class StickyBall {
         y: this.ball.pos.y - config.bounceRange * Math.sin(this.angleWithXRay)
       }
     }
+    if (this.quadrant === quadrantThree) {
+      rangeHead = {
+        x: this.ball.pos.x + config.bounceRange * Math.cos(this.angleWithXRay),
+        y: this.ball.pos.y - config.bounceRange * Math.sin(this.angleWithXRay)
+      }
+      rangeBack = {
+        x: this.ball.pos.x - config.bounceRange * Math.cos(this.angleWithXRay),
+        y: this.ball.pos.y + config.bounceRange * Math.sin(this.angleWithXRay)
+      }
+    }
+    if (this.quadrant === quadrantTwo) {
+      rangeBack = {
+        x: this.ball.pos.x + config.bounceRange * Math.cos(this.angleWithXRay),
+        y: this.ball.pos.y + config.bounceRange * Math.sin(this.angleWithXRay)
+      }
+      rangeHead = {
+        x: this.ball.pos.x - config.bounceRange * Math.cos(this.angleWithXRay),
+        y: this.ball.pos.y - config.bounceRange * Math.sin(this.angleWithXRay)
+      }
+    }
+    if (this.quadrant === quadrantOne) {
+      rangeBack = {
+        x: this.ball.pos.x + config.bounceRange * Math.cos(this.angleWithXRay),
+        y: this.ball.pos.y - config.bounceRange * Math.sin(this.angleWithXRay)
+      }
+      rangeHead = {
+        x: this.ball.pos.x - config.bounceRange * Math.cos(this.angleWithXRay),
+        y: this.ball.pos.y + config.bounceRange * Math.sin(this.angleWithXRay)
+      }
+    }
     // remove previous tweens if needed
     TWEEN.removeAll();
     // build the tween to go ahead
     var tweenHead	= new TWEEN.Tween(this.ball.pos)
       .to(rangeHead, 50)
       .easing(TWEEN.Easing.Elastic.Out)
-      .onUpdate(update)
     // build the tween to go backward
     var tweenBack	= new TWEEN.Tween(this.ball.pos)
       .to(rangeBack, 50)
       .easing(TWEEN.Easing.Elastic.Out)
-      .onUpdate(update)
     // after tweenHead do tweenBack
     tweenHead.chain(tweenBack)
     // after tweenBack do tweenHead, so it is cycling
@@ -146,11 +176,8 @@ class StickyBall {
         }
         stickyBall.ctx.clearRect(0, 0, stickyBall.ctx.canvas.width, stickyBall.ctx.canvas.height)
         stickyBall.drawBallOnCanvas(stickyBall.ball.pos.x, stickyBall.ball.pos.y, stickyBall.ball.radius, 0, Math.PI*2, stickyBall.ball.color)
-        console.log(stickyBall.ball.pos)
       }
       TWEEN.update(time)
-    }
-    function update() {
     }
   }
   dragMoveDraw(pos){
@@ -168,24 +195,13 @@ class StickyBall {
     } else {
       this.ball.radius = 5
     }
-    // 距离拉远
-    //if (curDis > this.preDis && this.ball.radius > config.minRadius) {
-    //  this.ball.radius -= 0.2
-    //}
-    //// 距离拉近
-    //if (curDis < this.preDis && this.ball.radius < config.radius) {
-    //  this.ball.radius = (this.ball.radius + 0.2) > config.radius ? config.radius : (this.ball.radius + 0.2)
-    //}
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height)
-    //this.drawBall(this.ball.pos.x, this.ball.pos.y, this.ball.radius, 0, Math.PI*2, this.ball.color)
-    //this.drawBall(pos.x, pos.y, config.radius, 0, Math.PI*2, config.gray)
     // 画两圆之间的连接线
     let angle = Math.atan(Math.abs(this.ball.pos.y - pos.y)/Math.abs(this.ball.pos.x - pos.x))
     let alpha = Math.acos((config.radius - this.ball.radius)/ distance(pos, this.ball.pos))
     let theta = Math.PI - alpha
-    // 重新算我就不信了
-    let pOneLeft, pOneRight, dragOneLeft, dragOneRight
-    if (pos.x >= this.ball.pos.x && pos.y > this.ball.pos.y) {
+    let pOneLeft, pOneRight, dragOneLeft, dragOneRight,startAngleOne ,startAngleTwo
+    if (pos.x >= this.ball.pos.x && pos.y > this.ball.pos.y) { // 第四象限
       this.quadrant = quadrantFour
       // 以原来的小圆圆心为中心的坐标系中，拖拽圆的圆心在第四象限
       if (angle + theta <= Math.PI) {
@@ -240,20 +256,128 @@ class StickyBall {
           y: pos.y - config.radius * Math.sin(chi)
         }
       }
-      let control = {
-        x: (this.ball.pos.x + pos.x)/2,
-        y: (this.ball.pos.y + pos.y)/2
-      }
-      this.ctx.beginPath()
-      this.ctx.strokeStyle = config.gray
-      this.ctx.fillStyle = config.gray
-      this.ctx.arc(this.ball.pos.x, this.ball.pos.y, this.ball.radius,angle + theta, angle + theta + Math.PI, false)
-      this.ctx.quadraticCurveTo(control.x, control.y, dragOneRight.x, dragOneRight.y)
-      this.ctx.arc(pos.x, pos.y, config.radius, Math.PI*2 - theta + angle, Math.PI*3 - theta + angle, false)
-      this.ctx.quadraticCurveTo(control.x, control.y, pOneLeft.x, pOneLeft.y)
-      this.ctx.stroke()
-      this.ctx.fill()
+      startAngleOne = angle + theta
+      startAngleTwo = Math.PI*2 - theta + angle
     }
+    if (pos.x < this.ball.pos.x && pos.y >= this.ball.pos.y) {
+      this.quadrant = quadrantThree // 第三象限
+      if(theta - angle <= Math.PI/2) {
+        let chi = theta - angle
+        pOneLeft = {
+          x: this.ball.pos.x - this.ball.radius * Math.cos(chi),
+          y: this.ball.pos.y - this.ball.radius * Math.sin(chi)
+        }
+      } else {
+        let chi = Math.PI - (theta - angle)
+        pOneLeft = {
+          x: this.ball.pos.x + this.ball.radius * Math.cos(chi),
+          y: this.ball.pos.y - this.ball.radius * Math.sin(chi)
+        }
+      }
+      if (angle + theta <= Math.PI) {
+        let chi = Math.PI - theta - alpha
+        pOneRight = {
+          x: this.ball.pos.x + this.ball.radius * Math.cos(chi),
+          y: this.ball.pos.y + this.ball.radius * Math.sin(chi)
+        }
+      } else {
+        let chi = angle + theta - Math.PI
+        pOneRight = {
+          x: this.ball.pos.x + this.ball.radius * Math.cos(chi),
+          y: this.ball.pos.y - this.ball.radius * Math.sin(chi)
+        }
+      }
+      if (angle + theta >= Math.PI) {
+        let chi = theta + angle - Math.PI
+        dragOneRight = {
+          x: pos.x + config.radius * Math.cos(chi),
+          y: pos.y - config.radius * Math.sin(chi)
+        }
+      } else {
+        let chi = Math.PI - angle - theta
+        dragOneRight = {
+          x: pos.x + config.radius * Math.cos(chi),
+          y: pos.y + config.radius * Math.sin(chi)
+        }
+      }
+      startAngleOne = theta - angle + Math.PI
+      startAngleTwo = Math.PI - theta - angle
+    }
+    if (pos.x <= this.ball.pos.x && pos.y < this.ball.pos.y) {
+      this.quadrant = quadrantTwo  // 第二象限
+      if(theta + angle >= Math.PI) {
+        let chi = theta + angle - Math.PI
+        pOneLeft = {
+          x: this.ball.pos.x + this.ball.radius * Math.cos(chi),
+          y: this.ball.pos.y + this.ball.radius * Math.sin(chi)
+        }
+      } else {
+        let chi = Math.PI - (theta + angle)
+        pOneLeft = {
+          x: this.ball.pos.x + this.ball.radius * Math.cos(chi),
+          y: this.ball.pos.y - this.ball.radius * Math.sin(chi)
+        }
+      }
+      if (theta - angle >= Math.PI/2) {
+        let chi = Math.PI - (theta - angle)
+        dragOneRight = {
+          x: pos.x + config.radius * Math.cos(chi),
+          y: pos.y + config.radius * Math.sin(chi)
+        }
+      } else {
+        let chi = theta - angle
+        dragOneRight = {
+          x: pos.x - config.radius * Math.cos(chi),
+          y: pos.y + config.radius * Math.sin(chi)
+        }
+      }
+      startAngleOne = Math.PI + theta + angle
+      startAngleTwo = Math.PI - (theta - angle)
+    }
+    if (pos.x > this.ball.pos.x && pos.y <= this.ball.pos.y) {
+      this.quadrant = quadrantOne  // 第一象限
+      if(theta - angle >= Math.PI/2) {
+        let chi = Math.PI - (theta - angle )
+        pOneLeft = {
+          x: this.ball.pos.x - this.ball.radius * Math.cos(chi),
+          y: this.ball.pos.y + this.ball.radius * Math.sin(chi)
+        }
+      } else {
+        let chi = theta - angle
+        pOneLeft = {
+          x: this.ball.pos.x + this.ball.radius * Math.cos(chi),
+          y: this.ball.pos.y + this.ball.radius * Math.sin(chi)
+        }
+      }
+      if (theta + angle >= Math.PI) {
+        let chi = theta + angle - Math.PI
+        dragOneRight = {
+          x: pos.x - config.radius * Math.cos(chi),
+          y: pos.y + config.radius * Math.sin(chi)
+        }
+      } else {
+        let chi = Math.PI - theta - angle
+        dragOneRight = {
+          x: pos.x - config.radius * Math.cos(chi),
+          y: pos.y - config.radius * Math.sin(chi)
+        }
+      }
+      startAngleOne = theta - angle
+      startAngleTwo = 2*Math.PI - theta - angle
+    }
+    let control = {
+      x: (this.ball.pos.x + pos.x)/2,
+      y: (this.ball.pos.y + pos.y)/2
+    }
+    this.ctx.beginPath()
+    this.ctx.strokeStyle = config.gray
+    this.ctx.fillStyle = config.gray
+    this.ctx.arc(this.ball.pos.x, this.ball.pos.y, this.ball.radius,startAngleOne, Math.PI + startAngleOne, false)
+    this.ctx.quadraticCurveTo(control.x, control.y, dragOneRight.x, dragOneRight.y)
+    this.ctx.arc(pos.x, pos.y, config.radius, startAngleTwo, Math.PI + startAngleTwo, false)
+    this.ctx.quadraticCurveTo(control.x, control.y, pOneLeft.x, pOneLeft.y)
+    this.ctx.stroke()
+    this.ctx.fill()
   }
   /**
    * 获取鼠标相对于canvas的坐标
